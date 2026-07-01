56a1ddd5a599d69aead749e0210f18696cd4a7d7
iwuavuozorjnr <crowndave31@gmail.com>
2026-07-01 14:30:13 -0700

    first portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+
+        nav.classList.add("scrolled");
+
+    }else{
+
+        nav.classList.remove("scrolled");
+
+    }
+
+});
